a496f5143a5f6686e66f3cce296732ef28995b6f
Pedro José Diego Dios <pedro.diegodios@gmail.com>
2017-05-03 16:44:32 +1000

    updated Client test

--- __tests__/Client.spec.js
+++ __tests__/Client.spec.js
@@ -34,7 +34,6 @@ describe('Coffee service (orders)', () => {
     afterEach(() => provider.verify())
 
     describe('lists no orders', () => {
-      // given:
       beforeEach(() =>
         provider.addInteraction({
           state: 'no orders',
@@ -54,9 +53,7 @@ describe('Coffee service (orders)', () => {
         })
       )
 
-      // when:
       it('', () => client.listOrders()
-      // then:
         .then((body) => {
           expect(body.orders).to.eql([])
         })
@@ -65,7 +62,6 @@ describe('Coffee service (orders)', () => {
     })
 
     describe('lists many orders', () => {
-      // given:
       beforeEach(() =>
         provider.addInteraction({
           state: 'many orders',
@@ -90,9 +86,7 @@ describe('Coffee service (orders)', () => {
         })
       )
 
-      // when:
       it('', () => client.listOrders()
-      // then:
         .then((body) => {
           expect(body.orders.length).to.eql(3)
 
@@ -103,5 +97,191 @@ describe('Coffee service (orders)', () => {
         .catch(fail)
       )
     })
+
+    describe('gets one order', () => {
+      beforeEach(() =>
+        provider.addInteraction({
+          state: 'order 2',
+          uponReceiving: 'request to get a specific order',
+          withRequest: {
+            method: 'GET',
+            path: '/order/2',
+            headers: requestHeaders
+          },
+          willRespondWith: {
+            status: 200,
+            headers: responseHeaders,
+            body: {
+              id: 2,
+              coffees: [],
+              name: 'Charlie Brown',
+              path: '/order/2'
+            }
+          }
+        })
+      )
+
+      it('', () => client.getOrder(2)
+        .then((body) => {
+          expect(body.id).to.eql(2)
+          expect(body.coffees.length).to.eql(0)
+          expect(body.name).to.eql('Charlie Brown')
+          expect(body.path).to.eql('/order/2')
+        })
+        .catch(fail)
+      )
+    })
+
+    describe('fails to get one order', () => {
+      beforeEach(() =>
+        provider.addInteraction({
+          state: 'no orders',
+          uponReceiving: 'request to get a specific order',
+          withRequest: {
+            method: 'GET',
+            path: '/order/3',
+            headers: requestHeaders
+          },
+          willRespondWith: {
+            status: 404,
+            headers: responseHeaders,
+            body: {
+              message: 'Order with id 3 not found',
+              path: '/order/3'
+            }
+          }
+        })
+      )
+
+      it('', () => client.getOrder(3)
+        .catch(error => {
+          expect(error.response.status).to.eql(404)
+        })
+      )
+    })
+
+    describe('names an order', () => {
+      beforeEach(() =>
+        provider.addInteraction({
+          state: 'empty order 4',
+          uponReceiving: 'request to change order name',
+          withRequest: {
+            method: 'PATCH',
+            path: '/order/4',
+            headers: requestHeaders,
+            body: {
+              name: 'Annie Hall'
+            }
+          },
+          willRespondWith: {
+            status: 200,
+            headers: responseHeaders,
+            body: {
+              id: 4,
+              path: '/order/4'
+            }
+          }
+        })
+      )
+
+      it('', () => client.updateOrder(4,'Annie Hall')
+        .then((body) => {
+          expect(body.id).to.eql(4)
+          expect(body.path).to.eql('/order/4')
+        })
+        .catch(fail)
+      )
+    })
+
+    describe('fails to name an order', () => {
+      beforeEach(() =>
+        provider.addInteraction({
+          state: 'no orders',
+          uponReceiving: 'request to change order name',
+          withRequest: {
+            method: 'PATCH',
+            path: '/order/5',
+            headers: requestHeaders,
+            body: {
+              name: 'Alvy Singer'
+            }
+          },
+          willRespondWith: {
+            status: 404,
+            headers: responseHeaders,
+            body: {
+              message: 'Order with id 5 not found',
+              path: '/order/5'
+            }
+          }
+        })
+      )
+
+      it('', () => client.updateOrder(5,'Alvy Singer')
+        .catch(error => {
+          expect(error.response.status).to.eql(404)
+        })
+      )
+    })
+
+    describe('cancels an order', () => {
+      beforeEach(() =>
+        provider.addInteraction({
+          state: 'empty order 6',
+          uponReceiving: 'request to cancel the order',
+          withRequest: {
+            method: 'DELETE',
+            path: '/order/6',
+            headers: requestHeaders
+          },
+          willRespondWith: {
+            status: 200,
+            headers: responseHeaders,
+            body: {
+              id: 6,
+              path: '/order/6'
+            }
+          }
+        })
+      )
+
+      it('', () => client.deleteOrder(6)
+        .then((body) => {
+          expect(body.id).to.eql(6)
+          expect(body.path).to.eql('/order/6')
+        })
+        .catch(fail)
+      )
+    })
+
+    describe('fails to cancel an order', () => {
+      beforeEach(() =>
+        provider.addInteraction({
+          state: 'no orders',
+          uponReceiving: 'request to cancel the order',
+          withRequest: {
+            method: 'DELETE',
+            path: '/order/7',
+            headers: requestHeaders
+          },
+          willRespondWith: {
+            status: 404,
+            headers: responseHeaders,
+            body: {
+              message: 'Order with id 7 not found',
+              path: '/order/7'
+            }
+          }
+        })
+      )
+
+      it('', () => client.deleteOrder(7)
+        .catch(error => {
+          expect(error.response.status).to.eql(404)
+        })
+      )
+    })
+
   })
+
 })
