1ceec3706e5c2aa5c88e08b8dea0f1c551bccf4a
Pedro José Diego Dios <pedro.diegodios@gmail.com>
2017-05-05 11:56:50 +1000

    Added Web platform using react-native-web

---
+++ index.web.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import CoffeeTodo from './CoffeeTodo';
+
+AppRegistry.registerComponent('CoffeeTodo', () => CoffeeTodo);
+AppRegistry.runApplication('CoffeeTodo', { rootTag: document.getElementById('react-app') });
